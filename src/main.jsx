b9aa733b54afa2332b6c0c588497337af9a642ba
Michael Hellon <kansasboy308@gmail.com>
2024-08-22 13:33:43 -0500

    fixing bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import './index.css';
 // these are the pages that are in the pages folder
-import App from './App';
+import App from './App.jsx';
 import Home from './components/Home.jsx';
 import Portfolio from './components/Portfolio.jsx';
-import Contact from './components/Contact';
-import About from './components/About';
+import Contact from './components/Contact.jsx';
+import About from './components/About.jsx';
 
 
 // Define the accessible routes, and which components respond to which URL
