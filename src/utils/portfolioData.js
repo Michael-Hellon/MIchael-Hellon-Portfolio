1997e9e44c4393534be092e1c8eaf9b00d6c6f14
Michael Hellon <kansasboy308@gmail.com>
2024-09-18 20:35:45 -0500

    updated portfolio projects

--- src/utils/portfolioData.js
+++ src/utils/portfolioData.js
@@ -3,6 +3,12 @@
 export const portfolioData = [
     { id: 1, name: "Model View Controller", github:"https://github.com/Michael-Hellon/14-Model-View-Controller-MVC-Tech-Blog", img: "https://michael-hellon.github.io/React-Portfolio/src/assets/images/MVC_screenshot.png", url: "https://one4-model-view-controller-mvc-tech-blog-ibzo.onrender.com/" , description: "This is a CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well. It follow the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication." },
     { id: 2, name: "Progressive Web Application" , github:"https://github.com/Michael-Hellon/Progressive-Web-Applications-PWA-Text-Editor", img: "https://michael-hellon.github.io/React-Portfolio/src/assets/images/JATE_screenshot.png" , url: "https://progressive-web-applications-pwa-text-6jq9.onrender.com/", description: "A single page text editor that runs in the browser, and will also function offline, that feature a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser. This application implement methods for getting and storing data to an IndexedDB database. This application is deployed to Render. " },  
-    { id: 3, name: "TaskAttacker" , github:"https://github.com/Michael-Hellon/TaskAttacker", img: "https://michael-hellon.github.io/React-Portfolio/src/assets/images/TaskAttacker.png", url: "https://taskattacker.onrender.com/", description: "A daily planner that allows the user to create, update and deletes tasks as well as start times for those tasks." }  
-
+    { id: 3, name: "TaskAttacker" , github:"https://github.com/Michael-Hellon/TaskAttacker", img: "https://michael-hellon.github.io/React-Portfolio/src/assets/images/TaskAttacker.png", url: "https://taskattacker.onrender.com/", description: "A daily planner that allows the user to create, update and deletes tasks as well as start times for those tasks." },  
+    { id: 4, name: "PetFinder", github:"https://github.com/Michael-Hellon/PetFinder", img: "https://michael-hellon.github.io/React-Portfolio/src/assets/images/PetFinder.png", url: "https://michael-hellon.github.io/PetFinder/", description: "This webapp will allow user to be able to find adoption centers nearby, animals held by centers, and directions to them."},
+    { id: 5, name: "NoSQL Social Network API", github: "https://github.com/Michael-Hellon/NoSQL-Social-Network-API", img: "https://michael-hellon.github.io/React-Portfolio/src/assets/images/NoSQL-Social-Networkin-api.png", description: "A MongoDB database that stores users inputs and displays it by the API route" },
+    { id: 6, name: "", github: "", img: "", description: "" },
 ]
+
+/* 
+ { id: , name: "", github: "", img: "", description: "" },
+*/
